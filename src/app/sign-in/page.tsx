e3819368e32911f047ef265e80759f1b392fda7f
Lance <jiexi@Lances-MacBook-Air.local>
2023-10-31 02:26:48 +0200

    styled pages for mobile & tablet

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,8 +3,8 @@ import SignIn from "../_components/sign-in";
 function SignUpPage() {
   return (
     <div className="flex h-screen w-full  bg-white">
-      <div className="h-full flex-1 border-r"></div>
-      <div className="flex h-full w-[500px] items-center justify-center">
+      <div className="hidden h-full flex-1 border-r lg:flex"></div>
+      <div className="flex h-full w-full items-center justify-center px-6 lg:w-[500px] lg:px-0">
         <SignIn />
       </div>
     </div>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,8 +3,8 @@ import SignUp from "../_components/sign-up";
 function SignUpPage() {
   return (
     <div className="flex h-screen w-full  bg-white">
-      <div className="h-full flex-1 border-r"></div>
-      <div className="flex h-full w-[500px] items-center justify-center">
+      <div className="hidden h-full flex-1 border-r lg:flex"></div>
+      <div className="flex h-full w-full items-center justify-center px-6 lg:w-[500px] lg:px-0">
         <SignUp />
       </div>
     </div>
