559f5ff0240b14c264a768530d167b3a08bbdae5
Lance <jiexi@Lances-MacBook-Air.local>
2023-11-01 14:23:20 +0200

    set up dashboard page

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DashboardPage() {
+  return (
+    <div className="flex min-h-screen w-full bg-white">
+      <div className="fixed flex h-full w-[300px] flex-col bg-red-500"></div>
+      <div className="min-h-screen flex-1 bg-green-500">hngjg</div>
+    </div>
+  );
+}
+
+export default DashboardPage;
