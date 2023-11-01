ce3c181bdf6beb7432981497d05e1c5078de2dbb
Lance <jiexi@Lances-MacBook-Air.local>
2023-11-01 18:21:51 +0200

    styled for mobile

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 function DashboardPage() {
   return (
     <div className="flex min-h-screen w-full bg-white">
-      <div className="fixed flex h-full w-[300px] flex-col border-r  bg-white shadow-2xl shadow-black">
+      <div className="fixed flex h-full w-[80px] flex-col items-center border-r bg-white  shadow-2xl shadow-black lg:w-[300px] lg:items-start">
         <AdminNav />
       </div>
       <div className="min-h-screen flex-1 bg-slate-100">hngjg</div>
--- src/app/_components/dashboard/admin-nav.tsx
+++ src/app/_components/dashboard/admin-nav.tsx
@@ -5,14 +5,14 @@ import Icon from "../icon";
 
 function AdminNav() {
   return (
-    <div className="flex w-full flex-col space-y-8 px-16 pt-10">
+    <div className="flex w-full flex-col space-y-8 pt-10 lg:px-16">
       <div className="">
-        <h2 className="text-2xl font-semibold tracking-widest text-black">
+        <h2 className="hidden text-2xl font-semibold tracking-widest text-black lg:block">
           DesignedBy
         </h2>
       </div>
-      <nav className="flex flex-col space-y-6">
-        <h3 className="font-medium uppercase tracking-widest text-black">
+      <nav className="flex flex-col items-center space-y-6 lg:items-start">
+        <h3 className="hidden font-medium uppercase tracking-widest text-black lg:block">
           Quick menu
         </h3>
         <ul className="flex flex-col space-y-7">
@@ -21,8 +21,8 @@ function AdminNav() {
               key={index}
               className="flex items-center space-x-2 text-gray-400 transition-all duration-300 ease-out hover:text-black"
             >
-              <Icon name={item.icon} size={20} />
-              <Link className="capitaliz" href={item.href}>
+              <Icon name={item.icon} size={24} />
+              <Link className="capitaliz hidden lg:block" href={item.href}>
                 {item.title}
               </Link>
             </li>
