72c811e065653e33c16d6a50564377493bdc3c96
Lance <jiexi@Lances-MacBook-Air.local>
2023-11-14 15:04:31 +0200

    added addToCart function

--- src/server/api/routers/cart.tsx
+++ src/server/api/routers/cart.tsx
@@ -6,4 +6,109 @@ import {
   protectedProcedure,
 } from "@/server/api/trpc";
 
-export const cartRouter = createTRPCRouter({});
+export const cartRouter = createTRPCRouter({
+  addToCart: protectedProcedure
+    .input(
+      z.object({
+        productId: z.number(),
+        size: z.string(),
+        quantity: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.userId;
+
+      // if user is logged in get cart from db
+      if (userId) {
+        const cart = await ctx.db.cart.findFirst({
+          where: {
+            userId: userId,
+          },
+          include: {
+            items: {
+              include: {
+                product: {
+                  include: {
+                    images: {
+                      select: {
+                        id: true,
+                        key: true,
+                      },
+                    },
+                    priceAndsize: {
+                      select: {
+                        id: true,
+                        price: true,
+                        size: true,
+                        quantity: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        if (cart) {
+          const cartItem = cart.items.find(
+            (item) =>
+              item.productId === input.productId && item.size === input.size,
+          );
+
+          if (cartItem) {
+            cartItem.quantity += input.quantity;
+            await ctx.db.cart.update({
+              where: {
+                id: cart.id,
+              },
+              data: {
+                items: {
+                  update: {
+                    where: {
+                      id: cartItem.id,
+                    },
+                    data: {
+                      quantity: cartItem.quantity,
+                    },
+                  },
+                },
+              },
+            });
+          } else {
+            await ctx.db.cart.update({
+              where: {
+                id: cart.id,
+              },
+              data: {
+                items: {
+                  create: {
+                    productId: input.productId,
+                    size: input.size,
+                    quantity: input.quantity,
+                  },
+                },
+              },
+            });
+          }
+        } else {
+          try {
+            await ctx.db.cart.create({
+              data: {
+                userId: userId,
+                items: {
+                  create: {
+                    productId: input.productId,
+                    size: input.size,
+                    quantity: input.quantity,
+                  },
+                },
+              },
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
+    }),
+});
