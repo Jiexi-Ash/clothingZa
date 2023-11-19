e0302432769e85fe5a65b2f8264191a88f39a169
Lance <jiexi@Lances-MacBook-Air.local>
2023-11-19 20:11:08 +0200

    updated cart component

--- src/app/_components/cart.tsx
+++ src/app/_components/cart.tsx
@@ -31,11 +31,15 @@ async function UserCart() {
     <Sheet>
       <SheetTrigger asChild>
         <Button
-          className="relative flex h-8  w-8 items-center justify-center rounded-lg  border border-white/50"
+          // className="relative flex h-8  w-8 items-center justify-center rounded-lg  border border-white/50"
+
+          className={`relative flex h-8  w-8 items-center justify-center rounded-lg  border border-white/50 ${
+            cart ? "bg-[#72FFFF]/50" : "bg-transparent"
+          }`}
           variant="ghost"
           size="icon"
         >
-          <Badge className="bg-primaryGreen absolute -top-2 left-3 flex h-4 w-4 items-center justify-center rounded-full p-0 text-[10px] text-white lg:left-4">
+          <Badge className="absolute -top-2 left-3 flex h-4 w-4 items-center justify-center rounded-full bg-black p-0 text-[10px] text-white lg:left-4">
             {itemsLength}
           </Badge>
           <ShoppingBagIcon className="h-5 w-5 text-white" />
